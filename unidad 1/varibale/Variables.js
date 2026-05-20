bc3c4cb8d618bb230a52afec2fad11becce5b82e
JBarcia-pancrasio <callatejohan2026@gmail.com>
2026-05-20 09:54:08 -0500

    Add files via upload

---
+++ unidad 1/varibale/Variables.js
@@ -0,0 +1,15 @@
+//let para crear varaible que no va a cambiar
+//const para crear una varaible que es constante
+//var realiza las dos cosas
+
+let nombre="Johann Damian Barcia Vargas" 
+console.log(nombre )
+//console para imprimir por consola
+const val1=1 
+//const no se puede cambiar a futuro
+console.log(val1)
+val1=2
+
+
+
+
